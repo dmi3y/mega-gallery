f886530387d949e18108a91c2686cfafa49be540
dmi3y <lapshukov@gmail.com>
2016-02-25 02:52:03 -0800

    closer step to success

--- scripts/build-gh-pages.sh
+++ scripts/build-gh-pages.sh
@@ -5,6 +5,10 @@ npm run compile-tmpl
 npm run compile-script
 
 cp build/script.js script.js
-cp build/main.css main.css
+cat build/main.css gh-pages/style/* > main.css
 
 node scripts/build-gh-pages.js
+
+# it's going to be deploy
+# git commit -am 'build gh pages'
+# git push -f
