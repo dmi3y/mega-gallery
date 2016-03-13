a589e126c56fbe1bd67560d98b259291c6e186a4
dmi3y <lapshukov@gmail.com>
2016-03-13 14:38:42 -0700

    package json tweaks, standard script formatting

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,94 +1,97 @@
 /**
  * MG gallery main script
+ *
  */
-$(function mgGallery() {
-    var delay = 200; //animation delay
-    var hm = {};
-    var itemClass = ".mg-item";
-    var imgClass = ".mg-item-image";
-    var movingClass = ".mg-moving";
-    var _movingClass = "mg-moving";
-    var currentClass = ".mg-current";
-    var _currentClass = "mg-current";
-    var teaserClass = ".mg-item-teaser";
-    // This god needs refactoring...
-    var hlpr = { //helper hash
-        init: function(cur, gallery) {
-            cur.add(cur.prev('li')).add(cur.next('li')).each(function(ix) {
-                var li = $(this);
-                hm[ix] = {
-                    l: li.css('left'),
-                    r: li.css('right'),
-                    t: li.css('top'),
-                    w: $(imgClass, li).css('width'),
-                    h: $(imgClass, li).css('height')
-                }; // shortcuted object instead of array for better readability
-                hm.r = hm[0];
-                hm.d = hm[1];
-                hm.l = hm[2]; //shortcuten for left, right and default
-            });
-            hm.lg = $(itemClass, gallery).length;
-            gallery.data('initData', hm);
-            return hm;
-        },
-        anim: function(wht, wre) {
-            wht.addClass(_movingClass).animate({
-                left: wre.l,
-                right: wre.r,
-                top: wre.t
-            }, delay).find(imgClass).animate({
-                width: wre.w,
-                height: wre.h
-            }, delay, function() {
-                wht.toggleClass(_currentClass).removeClass(_movingClass);
-            }).parents('li').find(teaserClass).fadeToggle(delay);
-        },
-        doclk: function(it, st, cur, gallery) {
-            var wro = gallery.data('initData');
-            this.anim(it, wro.d); //move new item to center (default position) and make current
-            this.anim(cur, wro[st]); //move current to its new position
-            gallery.trigger('galleryslide', st); //hook for helpers
-        },
-        cirle: function(it, st, cur, eds, gallery) {
-            var mi = $(st == 'r' ? eds[0] : eds[1]).removeAttr('style'); //reassign things so it looks like circle
-            if (st == 'r') {
-                it.after(mi);
-            } else {
-                it.before(mi);
-            }
-            this.doclk(it, st, cur, gallery);
+
+/* global $ */
+$(function mgGallery () {
+  var delay = 200 // animation delay
+  var hm = {}
+  var itemClass = '.mg-item'
+  var imgClass = '.mg-item-image'
+  var movingClass = '.mg-moving'
+  var _movingClass = 'mg-moving'
+  var currentClass = '.mg-current'
+  var _currentClass = 'mg-current'
+  var teaserClass = '.mg-item-teaser'
+  // This god needs refactoring...
+  var hlpr = { // helper hash
+    init: function (cur, gallery) {
+      cur.add(cur.prev('li')).add(cur.next('li')).each(function (ix) {
+        var li = $(this)
+        hm[ix] = {
+          l: li.css('left'),
+          r: li.css('right'),
+          t: li.css('top'),
+          w: $(imgClass, li).css('width'),
+          h: $(imgClass, li).css('height')
+        } // shortcuted object instead of array for better readability
+        hm.r = hm[0]
+        hm.d = hm[1]
+        hm.l = hm[2] // shortcuten for left, right and default
+      })
+      hm.lg = $(itemClass, gallery).length
+      gallery.data('initData', hm)
+      return hm
+    },
+    anim: function (wht, wre) {
+      wht.addClass(_movingClass).animate({
+        left: wre.l,
+        right: wre.r,
+        top: wre.t
+      }, delay).find(imgClass).animate({
+        width: wre.w,
+        height: wre.h
+      }, delay, function () {
+        wht.toggleClass(_currentClass).removeClass(_movingClass)
+      }).parents('li').find(teaserClass).fadeToggle(delay)
+    },
+    doclk: function (it, st, cur, gallery) {
+      var wro = gallery.data('initData')
+      this.anim(it, wro.d) // move new item to center (default position) and make current
+      this.anim(cur, wro[st]) // move current to its new position
+      gallery.trigger('galleryslide', st) // hook for helpers
+    },
+    cirle: function (it, st, cur, eds, gallery) {
+      var mi = $(st === 'r' ? eds[0] : eds[1]).removeAttr('style') // reassign things so it looks like circle
+      if (st === 'r') {
+        it.after(mi)
+      } else {
+        it.before(mi)
+      }
+      this.doclk(it, st, cur, gallery)
+    }
+  }
+
+  $.fn.mgGallery = function () {
+    var gallery = $(this)
+    var it, eds
+
+    if ($(itemClass, gallery).length > 3) {
+      var cur = gallery.find(itemClass).filter(':nth-child(2)')
+      cur.addClass(_currentClass)
+      var mga = gallery.attr('data-merrygoround') || 'on'
+      $('[data-navigation]', gallery).click(function () { // catching prev next buttons
+        cur = $(currentClass, gallery)
+        if ($(movingClass, gallery).is('li')) return // ban click on animation
+        eds = $(itemClass, gallery).filter(':first, :last')
+        gallery.data('initData') || hlpr.init(cur, gallery) // get the initial values from the CSS
+        var st = $(this).data('navigation')
+        switch (st) {
+          case 'l':
+            it = cur.prev()
+            break
+          case 'r':
+            it = cur.next()
+            break
         }
-    };
-
-    $.fn.mgGallery = function() {
-        var gallery = $(this),
-            it, st, eds;
-
-        if ( $(itemClass, gallery).length > 3 ) {
-            var cur = gallery.find(itemClass).filter(":nth-child(2)");
-            cur.addClass(_currentClass);
-            var mga = gallery.attr('data-merrygoround') || 'on';
-            $('[data-navigation]', gallery).click(function() { //catching prev next buttons
-                cur = $(currentClass, gallery);
-                if ($(movingClass, gallery).is('li')) return; //ban click on animation
-                eds = $(itemClass, gallery).filter(":first, :last");
-                gallery.data('initData') || hlpr.init(cur, gallery); //get the initial values from the CSS
-                var st = $(this).data('navigation');
-                switch (st) {
-                    case 'l':
-                        it = cur.prev();
-                        break;
-                    case 'r':
-                        it = cur.next();
-                        break;
-                }
-                if (it.is(eds)) gallery.trigger('beforegalleryslideend', st); //hook for helpers @REV question for optimization
-                if (it.is(eds) && mga == 'on') { //last items hook and check for merrygoround functionality
-                    hlpr.cirle(it, st, cur, eds, gallery); /* here before animating might be done checking and ajax requests for more images either circling like now */
-                } else if (it.is('li')) { //normal rotation
-                    hlpr.doclk(it, st, cur, gallery);
-                }
-            });
-        } 
-    };
-});
+        if (it.is(eds)) gallery.trigger('beforegalleryslideend', st) // hook for helpers @REV question for optimization
+        if (it.is(eds) && mga === 'on') { // last items hook and check for merrygoround functionality
+          hlpr.cirle(it, st, cur, eds, gallery) /* here before animating might be done checking and ajax requests for more images either circling like now */
+        } else if (it.is('li')) { // normal rotation
+          hlpr.doclk(it, st, cur, gallery)
+        }
+      })
+    }
+  }
+})
