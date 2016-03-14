8882f84db66c73a9fb190b76771ca8135d12e654
dmi3y <lapshukov@gmail.com>
2016-03-14 01:45:43 -0700

    tasks for npm run...

---
+++ tasks/compile-templates.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash -x
+
+mkdir -p build/templates
+
+# All templates compiles to partials
+handlebars -e hbs --commonjs handlebars -p -f build/templates/partials-common.js src/templates/*.hbs
+handlebars -e hbs --amd -p -f build/templates/partials-amd.js src/templates/*.hbs
+handlebars -e hbs -p -f build/templates/partials.js src/templates/*.hbs
