3174afb102f34b9421dee84d9480314de35e2759
dmi3y <lapshukov@gmail.com>
2016-03-13 21:13:11 -0700

    watch source templates

--- bin/develop-gh-pages.sh
+++ bin/develop-gh-pages.sh
@@ -4,6 +4,6 @@ npm run watch-styles & /
 sane 'cat build/*.css gh-src/styles/* > index.css' --glob=build/*.css --glob=gh-src/styles/* & /
 npm run watch-scripts & /
 sane 'cat build/*.js gh-src/scripts/* > index.js' --glob=build/*.js --glob=gh-src/scripts/* & /
-sane 'node bin/build-gh-pages.js' --glob=gh-src/templates/**/*.hbs
+sane 'node bin/build-gh-pages.js' --glob=gh-src/templates/**/*.hbs --glob=src/templates/**/*.hbs
 
 trap 'kill %1; kill %2; kill %3; kill %4' SIGINT
