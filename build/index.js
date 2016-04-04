77996fc6c27eb30eb05c3ad27c272bdc497949b6
dmi3y <lapshukov@gmail.com>
2016-04-03 19:24:44 -0700

    builds to repo

---
+++ build/index.js
@@ -0,0 +1,56 @@
+// Autoscroll functionality
+
+/* global $ */
+$(function slidersAutoscroll () {
+  // selector might be modified as *[class^=gallery] to support all future possible scripts
+  $('[data-autoscroll]').each(function () {
+    var el = $(this)
+    var l = $('*[class^=arw-l]', el)
+    var r = $('*[class^=arw-r]', el)
+    var amt = r
+    var time = el.attr('data-autoscroll')
+    var dir = el.attr('data-autoscrolldir') || 'ltr'
+
+    switch (dir) {
+      case 'ltr':
+        amt = r
+        break
+      case 'rtl':
+        amt = l
+        break
+    }
+    var flg = true // flag to check reverse autoplay
+    el.on('galleryslide', function () { // catching prev next buttons with custom event
+      flg = false // everytime the gallery srolled flag goes to false
+    })
+    setInterval(function () {
+      if (el.hasClass('paused')) return
+      amt.click()
+      if (flg) {
+        amt = (amt === r) ? l : r // change direction if we could not autoscroll futher
+        amt.click() // and instantly click to not have double pause
+      }
+      flg = true // set flag back to true
+    }, time)
+    el.on('hover touchstart touchend', function () {
+      el.toggleClass('paused')
+    })
+  })
+})
+/* global $ */
+$(function mgGallery () {
+  $.fn.mgGallery = function () {
+    var gallery = $(this)
+    var inner = gallery.find('.mg-inner')
+
+    var navigation = gallery.find('[data-navigation]')
+    navigation.click(function (ev) {
+      var dir = this.dataset.navigation
+      var firstChild = inner.find('li:first-child')
+      var lastChild = inner.find('li:last-child')
+
+      if (dir === 'left') lastChild.before(firstChild)
+      else if (dir === 'right') firstChild.after(lastChild)
+    })
+  }
+})
---
+++ build/templates/common.js
@@ -0,0 +1,30 @@
+var Handlebars = require("handlebars");  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+Handlebars.partials['mg-body'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = container.invokePartial(partials["mg-item"],depth0,{"name":"mg-item","data":data,"indent":"\t\t\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div class=\"mg-body\">\n"
+    + ((stack1 = container.invokePartial(partials["mg-navigation"],depth0,{"name":"mg-navigation","data":data,"indent":"\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "	<ul class=\"mg-inner\">\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "	</ul>\n</div>\n";
+},"usePartial":true,"useData":true});
+Handlebars.partials['mg-item'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "<li class=\"mg-item\">\n    <div class=\"mg-item-teaser\">\n        <h3 class=\"mg-item-header\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.teaser : depth0)) != null ? stack1.header : stack1), depth0))
+    + "</h3>\n        <div class=\"mg-item-text\">\n            "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.teaser : depth0)) != null ? stack1.text : stack1), depth0))
+    + "\n        </div>\n    </div>\n    <div class=\"mg-item-image\">\n        <img src=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.image : depth0)) != null ? stack1.src : stack1), depth0))
+    + "\" alt=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.image : depth0)) != null ? stack1.alt : stack1), depth0))
+    + "\" />\n    </div>\n</li>\n";
+},"useData":true});
+Handlebars.partials['mg-navigation'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<span data-navigation=\"r\" class=\"mg-navigate-right\"></span>\n<span data-navigation=\"l\" class=\"mg-navigate-left\"></span>\n";
+},"useData":true});
