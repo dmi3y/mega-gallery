47f523b37e5a3576e5462f24145b9a4e11c0ff01
dmi3y <lapshukov@gmail.com>
2016-04-20 23:25:54 -0700

    refering to the node_modules jquery

--- tasks/develop-gh-pages.sh
+++ tasks/develop-gh-pages.sh
@@ -3,7 +3,7 @@
 npm run watch-styles & /
 sane 'cat build/*.css gh-src/styles/* > index.css' --glob=build/*.css --glob=gh-src/styles/* & /
 npm run watch-scripts & /
-cat_scripts='cat bower_components/jquery/dist/jquery.slim.js build/*.js gh-src/scripts/* > index.js'
+cat_scripts='cat node_modules/jquery/dist/jquery.slim.js build/*.js gh-src/scripts/* > index.js'
 sane $cat_scripts --glob=build/*.js --glob=gh-src/scripts/* & /
 sane 'node tasks/build-gh-pages.js' --glob=gh-src/templates/*.hbs --glob=src/templates/**/*.hbs
 
